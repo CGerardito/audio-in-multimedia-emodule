922d599c7ab83134b8bbfd513210da316c8ebd58
Christopher Gerardito <100307373+CGerardito@users.noreply.github.com>
2026-06-30 11:49:39 +0700

    Fixed base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
+  base: '/audio-in-multimedia-emodule',
   server: { port: 5173 },
 })
